98956650d6454e337a76b8e5d149272261ebe182
Ben Iofel <iofelben@gmail.com>
2017-04-20 18:01:04 -0400

    fix indentation

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
+// vim: set et sw=2 ts=2 sts=2:
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   if (argc != 5) {
-        fprintf(stderr, "Usage %s\n"
-                        "\t<input files storing information on files>\n"
-                        "\t<input files storing information on directories>\n"
-                        "\t<disk size>\n"
-                        "\t<block size>\n",
-                    argv[0]);
-        return 1;
-    }
+    fprintf(stderr, "Usage %s\n"
+                    "\t<input files storing information on files>\n"
+                    "\t<input files storing information on directories>\n"
+                    "\t<disk size>\n"
+                    "\t<block size>\n",
+                argv[0]);
+    return 1;
+  }
   return 0;
 }
