eb23fd7a706461f17a936faf23502f3085e528b6
Phil Fishbein <phil@fishbe.in>
2017-04-20 18:51:19 -0400

    try to parse files

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 // vim: set et sw=2 ts=2 sts=2:
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void parseFileList(FILE* file) {
+  int fileSize = 0;
+  int line = 1;
+  char filePath[100000];
+  int ret = -1;
+  while ((ret = fscanf(file, " %*i %*i %*s %*i %*s %*s %i %*s %*i %*i:%*i %s\n", &fileSize, filePath)) != EOF) {
+    printf("Line %i\nFile Size: %i\nFile Path: %s\n", line, fileSize, filePath);
+    line++;
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 5) {
@@ -12,5 +25,6 @@ int main(int argc, char *argv[]) {
                 argv[0]);
     return 1;
   }
-  return 0;
+  FILE *f = fopen(argv[1], "r");
+  parseFileList(f);
 }
